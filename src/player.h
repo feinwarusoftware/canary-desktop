d096423b817194a70e4192c5e4c689d09a12b34d
Salies <serezane@protonmail.com>
2019-12-04 21:35:29 -0300

    Moved to QWebEngine as framework.

--- src/player.h
+++ src/player.h
@@ -4,35 +4,27 @@
 #include "bass.h"
 #include "bassmix.h"
 
-#include "covers.h"
-
-#include <QVector>
-#include <QString>
-
-#include <vector>
-
 #include <QObject>
+#include <QVector>
 #include <QVariant> //TODO: replace for GUI or shit later on? ===> doing it progressively
 
 class Player : public QObject {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	explicit Player(QObject* parent = 0);
-	void init(QObject* r);
-	Q_INVOKABLE void insertToQueue(int pos, char const* song);
+    explicit Player(QObject* parent = 0);
+    void init(QObject* r);
+    void insertToQueue(int pos, char const* song);
 	Q_INVOKABLE bool loadSong(int pos);
-	bool clearMixer();
-	Q_INVOKABLE bool jump(int direction);
-	Q_INVOKABLE bool play();
-	Q_INVOKABLE bool changeVolume(float v);
-	Q_INVOKABLE bool active();
-	Q_INVOKABLE bool pause(bool drag = 0);
-	Q_INVOKABLE bool seek(int to, int width);
-	Q_INVOKABLE int getPosition();
-	Q_INVOKABLE QString toMinHourFormat(int bytes);
-	Q_INVOKABLE int bytesToSeconds(int bytes);
-	bool setInfo(const char* songDir);
-	Q_INVOKABLE bool jumpToSong(int pos);
+    Q_INVOKABLE double getPositionInSeconds();
+    Q_INVOKABLE bool play();
+    Q_INVOKABLE bool pause();
+    Q_INVOKABLE bool changeVolume(float v);
+	Q_INVOKABLE bool seek(double to);
+	Q_INVOKABLE bool jump(bool direction);
+
+private:
+	int getCSLengthInSeconds();
+	bool loadPlugins();
 
 signals:
 
